16dca9d541402c2816ca75b9d018576cf3f70169
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2024-10-10 17:30:42 +0100

    week-5-assignment

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const mysql = require('mysql2')
+const 
+
+// Question 1
+
+
+
+
+
+// listen to the server
+const PORT = 3000
+app.listen(PORT, () => {
+ console.log(`server is running on http://localhost:${PORT}`);
+ 
+})
